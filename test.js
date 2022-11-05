082d3e5d35417e10ddde2a1f173c9d8d20709624
Pei Zhang <peip7@outlook.com>
2022-11-06 09:39:37 +1300

    refactor

--- test.js
+++ test.js
@@ -3,12 +3,6 @@ import request from 'supertest'
 const url =
   'https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false'
 
-describe('sample test', () => {
-  it('test that true === true', () => {
-    expect(true).toBe(true)
-  })
-})
-
 describe('Endpoint Test', () => {
   it('should return a 200 status code', async () => {
     const res = await request(url).get('/')
@@ -16,7 +10,7 @@ describe('Endpoint Test', () => {
   })
 })
 
-describe('Get name', () => {
+describe('Get Name', () => {
   it('should test Name === "Badges"', async () => {
     const res = await request(url).get('/')
     expect(res.body.Name).toBe('Badges')
