c4ad0a716c8a7e72ce9c394934be037082e270e1
Pei Zhang <peip7@outlook.com>
2022-11-05 20:11:09 +1300

    pass sample test

---
+++ test.js
@@ -0,0 +1,10 @@
+import { request } from 'supertest'
+
+const baseUrl =
+  'https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false'
+
+describe('sample test', () => {
+  it('test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
