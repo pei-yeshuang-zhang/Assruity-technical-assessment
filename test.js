684d89cf12226d5a29549f8684bf41cc741dde28
Pei Zhang <peip7@outlook.com>
2022-11-05 21:04:15 +1300

    pass endpoint test

--- test.js
+++ test.js
@@ -1,10 +1,17 @@
-import { request } from 'supertest'
+import request from 'supertest'
 
-const baseUrl =
+const url =
   'https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false'
 
 describe('sample test', () => {
   it('test that true === true', () => {
     expect(true).toBe(true)
+  })
+})
+
+describe('Endpoint Test', () => {
+  it('should return a 200 status code', async () => {
+    const res = await request(url).get('/')
+    expect(res.statusCode).toBe(200)
   })
 })
